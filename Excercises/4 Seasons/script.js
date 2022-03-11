cfd57f3bae5cca79f422081e9c778b40ab4990d4
Kara-B <kjbutler819@verizon.net>
2022-03-10 19:11:33 -0500

    4 Seasons Updated

--- Excercises/4 Seasons/script.js
+++ Excercises/4 Seasons/script.js
@@ -1,21 +1,24 @@
 
 
-const buttonElement = document.querySelector('.js-button')
+const buttonElement = document.querySelector('.js-button');
 
 buttonElement.addEventListener('click', function(){
+const birthdayTime = document.querySelector('.js-birthday-time');
+const userBirthday = document.querySelector('.js-birthday-input');
+const birthdaySeason = document.querySelector('.js-season');
 
-const userBirthday = document.querySelector('.js-birthday-input')
-const birthdaySeason = document.querySelector('.js-season')
-// let birthdayTime= document.querySelector('js-birthday-time')
+birthdayTime.innerHTML = (userBirthday.value);
+
 var dateObj = new Date(userBirthday.value);
 let month = dateObj.getUTCMonth() + 1; 
 let day = dateObj.getUTCDate();
 
-    if ((month == 11 && day >= 21) || (month==0) || (month==1)||(month==2 && day<=20)) {
+
+    if ((month == 12 && day >= 21) || (month==1) || (month==2)||(month==3 && day<=20)) {
         birthdaySeason.innerHTML = `Winter`;}
-    else if ((month == 2 && day >= 20) || (month==3) || (month==4)||(month==5 && day<=21))   {
+    else if ((month == 3 && day >= 20) || (month==4) || (month==5)||(month==6 && day<=21))   {
         birthdaySeason.innerHTML = `Spring`;
-    } else if ((month == 5 && day >= 22) || (month==6) || (month==7)||(month==8 && day<=22)) {
+    } else if ((month == 6 && day >= 22) || (month==7) || (month==8)||(month==9 && day<=22)) {
         birthdaySeason.innerHTML = `Summer`;
     }else {
         birthdaySeason.innerHTML = `Fall`;
